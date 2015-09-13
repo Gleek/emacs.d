97c6ebc5a623c98c134cbc0504b216ed0af53ff5
Umar Ahmad <ahmad.umar2008@yahoo.in>
2015-09-14 02:45:31 +0530

    Update

--- elpa/helm-gtags-20150913.639/helm-gtags-autoloads.el
+++ elpa/helm-gtags-20150913.639/helm-gtags-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "helm-gtags" "helm-gtags.el" (21990 63905 0
+;;;### (autoloads nil "helm-gtags" "helm-gtags.el" (22005 57855 0
 ;;;;;;  0))
 ;;; Generated autoloads from helm-gtags.el
 
