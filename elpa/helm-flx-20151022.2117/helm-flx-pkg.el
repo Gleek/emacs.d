657975770105a975771b28e8e0729e6663839460
Umar Ahmad <ahmad.umar2008@yahoo.in>
2015-10-28 22:37:36 +0530

    Update

--- elpa/helm-flx-20151022.2117/helm-flx-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "helm-flx" "20151022.2117" "Sort helm candidates by flx score" '((emacs "24.4") (helm "1.7.9") (flx "0.5")) :url "https://github.com/PythonNut/helm-flx" :keywords '("convenience" "helm" "fuzzy" "flx"))
