5a13d25df497fff8941d311870f72db1bc3d5c81
Umar Ahmad <Gleek@users.noreply.github.com>
2021-08-03 12:40:11 +0530

    Bind shortdoc to a keybinding

--- core/core-tools.el
+++ core/core-tools.el
@@ -466,6 +466,9 @@ the currently playing track."
           (b "B / 8" "Bit"))
         math-units-table nil))
 
+(use-package shortdoc :ensure nil
+  :bind ("C-h s" . shortdoc-display-group))
+
 
 (use-package profiler
   :bind (("<C-f12>" . profiler-start)
