3bf6e099c38551cadf9882b1021ffb93ca739f16
Umar Ahmad <Gleek@users.noreply.github.com>
2021-11-05 02:32:57 +0530

    Modify tramp settings to make them similar to doom

--- core/core-tramp.el
+++ core/core-tramp.el
@@ -6,6 +6,12 @@
   (setq tramp-persistency-file-name (concat CACHE-DIR "tramp"))
   ;; (setq tramp-shell-prompt-pattern "^[^$>\n]*[#$%>] *\\(\[[0-9;]*[a-zA-Z] *\\)*")
   :config
+  (setq remote-file-name-inhibit-cache 60
+        tramp-verbose 1
+        vc-ignore-dir-regexp (format "%s\\|%s\\|%s"
+                                     vc-ignore-dir-regexp
+                                     tramp-file-name-regexp
+                                     "[/\\\\]node_modules"))
   (add-to-list 'tramp-default-proxies-alist
                '("jobs" nil "/ssh:entry:"))
   (add-to-list 'tramp-default-proxies-alist
