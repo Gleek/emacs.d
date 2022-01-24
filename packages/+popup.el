6990bbb27cce183c13d72200864c768fc7f34341
Umar Ahmad <Gleek@users.noreply.github.com>
2022-01-24 15:46:15 +0530

    Treat async shell command output as popup

--- packages/+popup.el
+++ packages/+popup.el
@@ -920,6 +920,7 @@ prevent the popup(s) from messing up the UI (or vice versa)."
      :slot 2 :vslot 2 :size 0.45 :select t))
   '(("^\\*Warnings" :vslot 99 :size 0.25)
     ("^\\*Backtrace" :vslot 99 :size 0.4 :quit nil)
+    ("^\\*Async Shell Command*" :vslot 99 :size 0.4 :quit t)
     ("^\\*CPU-Profiler-Report "    :side bottom :vslot 100 :slot 1 :height 0.4 :width 0.5 :quit nil)
     ("^\\*Memory-Profiler-Report " :side bottom :vslot 100 :slot 2 :height 0.4 :width 0.5 :quit nil)
     ("^\\*Process List\\*" :side bottom :vslot 101 :size 0.25 :select t :quit t)
