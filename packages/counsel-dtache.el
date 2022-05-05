5fa213785e16c89e4c0891ab6a66c1a4e6b9c719
Umar Ahmad <Gleek@users.noreply.github.com>
2022-05-06 00:40:30 +0530

    counsel-dtache -- Attach to any session

--- packages/counsel-dtache.el
+++ packages/counsel-dtache.el
@@ -20,22 +20,26 @@
             :require-match t
             :action '(1
                       ("o" counsel-dtache--open-session "Open")
+                      ("a" counsel-dtache--attach-session "Attach")
                       ("k" counsel-dtache--kill-and-delete "Kill")
                       ("s" counsel-dtache--kill "Stop")
                       ("t" counsel-dtache--tail "Tail")
                       ("R" counsel-dtache--rerun "Rerun")
                       ("d" counsel-dtache--delete "Delete"))
-            :caller 'counsel-dtache)
-  )
+            :caller 'counsel-dtache))
 
 (defun counsel-dtache--get-sessions()
   (dtache-session-candidates (dtache-get-sessions)))
+
 
 (defun counsel-dtache--decode(cand)
   (cdr cand))
 
 (defun counsel-dtache--open-session(cand)
   (dtache-open-session (counsel-dtache--decode cand)))
+
+(defun counsel-dtache--attach-session(cand)
+  (dtache-attach-session (counsel-dtache--decode cand)))
 
 (defun counsel-dtache--kill(cand)
   (dtache-kill-session (counsel-dtache--decode cand)))
