da74cfef4006decf2f57ae9311d0bbd5583c31f6
Umar Ahmad <Gleek@users.noreply.github.com>
2022-05-14 03:38:08 +0530

    core-metrics remove mouse and insert tracking

--- core/core-metrics.el
+++ core/core-metrics.el
@@ -21,9 +21,11 @@
   (keyfreq-autosave-mode 1)
   (setq keyfreq-excluded-commands
         '(self-insert-command
+          org-self-insert-command
           next-line
           previous-line
-          mwheel-scroll)))
+          mwheel-scroll
+          lsp-ui-doc--handle-mouse-movement)))
 
 
 ;; Requires https://activitywatch.net/
@@ -53,6 +55,13 @@
          ("<C-S-f12>" . profiler-report)
          ("<M-f12>" . profiler-stop)))
 
+
+(use-package mx-metrics
+  :ensure nil
+  :defer 2
+  :config
+  (setq mx-metrics-file (expand-file-name "mx-metrics" CACHE-DIR))
+  (mx-metrics-mode t))
 
 (provide 'core-metrics)
 ;;; core-metrics.el ends here
