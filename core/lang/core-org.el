2d21b623eaa4b7a5bebd27415a2cd24de50f7195
Umar Ahmad <Gleek@users.noreply.github.com>
2023-04-27 19:01:58 +0530

    Org prettify checkboxes

--- core/lang/core-org.el
+++ core/lang/core-org.el
@@ -98,6 +98,23 @@
                        (file-name-nondirectory (buffer-file-name))
                        (line-number-at-pos)))))
 
+  (defun org-beautify-checkbox ()
+    "Beautify Org Checkbox Symbol"
+    (push '("[ ]" .  "☐") prettify-symbols-alist)
+    (push '("[X]" . "☒" ) prettify-symbols-alist)
+    (push '("[-]" . "❍" ) prettify-symbols-alist)
+    (prettify-symbols-mode))
+  (defface org-checkbox-done-text
+    '((t (:foreground "#71696A" :strike-through t)))
+    "Face for the text part of a checked org-mode checkbox.")
+
+  (font-lock-add-keywords
+   'org-mode
+   `(("^[ \t]*\\(?:[-+*]\\|[0-9]+[).]\\)[ \t]+\\(\\(?:\\[@\\(?:start:\\)?[0-9]+\\][ \t]*\\)?\\[\\(?:X\\|\\([0-9]+\\)/\\2\\)\\][^\n]*\n\\)"
+      1 'org-checkbox-done-text prepend))
+   'append)
+  (add-hook 'org-mode-hook 'org-beautify-checkbox)
+
 
   (defun +capture-inbox()
     (interactive)
