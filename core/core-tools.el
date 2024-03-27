25ea87d3d93d7b6c2c18acb9904b6740c52de6f3
Umar Ahmad <Gleek@users.noreply.github.com>
2024-03-27 18:08:26 +0530

    Fix paradox to work with vc

--- core/core-tools.el
+++ core/core-tools.el
@@ -219,10 +219,19 @@
   :init
   (defvar paradox-automatically-star)
   (defvar paradox-execute-asynchronously)
-  (setq paradox-automatically-star t)
-  (setq paradox-execute-asynchronously t))
-
-;; (use-package debbugs)
+  (setq paradox-automatically-star nil)
+  (setq paradox-execute-asynchronously t)
+  :config
+  (defconst paradox--package-count
+    '(("total" . 0) ("built-in" . 0)
+      ("obsolete" . 0) ("deleted" . 0)
+      ("available" . 0) ("new" . 0)
+      ("held" . 0) ("disabled" . 0)
+      ("dependency" . 0) ("avail-obso" . 0)
+      ("incompat" . 0)  ("external" . 0)
+      ("installed" . 0) ("unsigned" . 0)
+      ;; Add source manually to the paradox list to handle vc packages
+      ("source" . 0))))
 
 (use-package transient
   :init
