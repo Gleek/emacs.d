873adfd0ff5eabb7ab8b8235a05859e29cd703df
Umar Ahmad <Gleek@users.noreply.github.com>
2024-06-08 17:19:14 +0530

    Experiment with ifacemaker to quickly generate interfaces for struct

--- core/lang/core-go.el
+++ core/lang/core-go.el
@@ -8,7 +8,7 @@
 ;;  go install golang.org/x/tools/cmd/goimports@latest
 ;;  go install golang.org/x/tools/cmd/gorename@latest
 ;;  DEPRECATED: go get -u golang.org/x/tools/cmd/guru
-;;
+;;  go install github.com/vburenin/ifacemaker@latest
 ;;
 ;; https://golangci-lint.run/usage/install/#local-installation
 ;; brew install golangci-lint
@@ -37,6 +37,42 @@
             (eq major-mode 'go-ts-mode))
     (gofmt)))
 
+(defun go-guess-package-name()
+  "Guess the package name of the current buffer."
+  (save-excursion
+    (goto-char (point-min))
+    (if (re-search-forward "^package[ \t]+\\([a-zA-Z0-9_]+\\)" nil t)
+        (match-string 1)
+      nil)))
+
+
+(defun +generate-interface(&optional arg)
+  "Generate interface from struct using ifacemaker.
+Requires go install github.com/vburenin/ifacemaker@latest.
+Call with prefix arg to create a new buffer with the interface definition."
+  (interactive "P")
+  (let* ((struct-name (thing-at-point 'symbol))
+         (package-name (if arg (read-string "Package name: " (go-guess-package-name)) "dummy"))
+         (interface-name (read-string "Interface name: " (format "I%s" struct-name)))
+         (file-name (buffer-file-name))
+         (shell-command (format "ifacemaker -f=%s -s=%s -i=%s  -p=%s -c=' '" file-name struct-name interface-name package-name))
+         (output (shell-command-to-string shell-command))
+         (output (substring output (string-match "package " output)))
+         ;; Just get string from the "interface" line
+         (interface (substring output (string-match "type.*interface" output))))
+    ;; If called with prefix arg, create a new buffer otherwise just insert the interface definition in current buffer
+    (if arg
+        (with-current-buffer (get-buffer-create (format "*interface-%s" struct-name))
+          (erase-buffer)
+          (go-ts-mode)
+          (insert output)
+          (goto-char (point-min))
+          (display-buffer (current-buffer)))
+      (progn
+        ;; Go to beginning of line and insert the interface
+        (beginning-of-line)
+        (insert (format "%s" interface))))))
+
 (use-package go-tag
   :after (go-ts-mode)
   :demand t
@@ -54,7 +90,8 @@
   :ensure nil
   :mode "\\.go\\'"
   :bind (:map go-ts-mode-map
-              ("C-z a" . +go-tag-add))
+              ("C-z a" . +go-tag-add)
+              ("C-z i" . +generate-interface))
   :config
   (add-hook 'go-ts-mode-hook (lambda() (setq-local format-all-formatters '(("Go" goimports)))))
   (add-hook 'go-ts-mode-hook (lambda() (require 'dap-dlv-go)))
@@ -84,6 +121,8 @@
   (setq go-playground-basedir (expand-file-name "src/example.com/playground/" (getenv "GOPATH"))))
 
 (use-package flycheck-golangci-lint
+  ;; Not needed separately as handled by https://github.com/nametake/golangci-lint-langserver
+  :disabled t
   :hook ((go-mode go-ts-mode) . +go-setup-checkers)
   :config
   (add-to-list 'flycheck-checker-max-level '(golangci-lint . warning))
