288362f456694982f87f1019c1d288fc9d475df4
Umar Ahmad <Gleek@users.noreply.github.com>
2024-12-29 17:24:13 +0530

    Have both daemon and non-daemon launchers through script

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if ! emacsclient -s launcher -e '(message "Launcher daemon running")' &>/dev/null; then
+SERVER_NAME="server" # pass server / launcher for primary or dedicated daemon respectively
+ELISP_CMD="(+launch-default-launcher)"
+if ! emacsclient -s "$SERVER_NAME" -e '(message "Launcher daemon running")' &>/dev/null; then
     ~/.config/emacs/scripts/launcher-daemon.sh
     sleep 1
 fi
 
-(emacsclient -s launcher -c -e "(+launch-default-launcher)" &)
+if emacsclient -s "$SERVER_NAME" -e "
+ (if (daemonp) nil
+     (error \"\"))
+"; then
+    # if it's a daemon a new frame will be created
+    (emacsclient -s "$SERVER_NAME" -c -e "$ELISP_CMD" &)
+else
+    (emacsclient -s "$SERVER_NAME" -e "$ELISP_CMD" &)
+fi
