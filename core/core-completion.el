76b8c1b9b0cc434f7e138b391aa5b4ad1000c0d5
Umar Ahmad <Gleek@users.noreply.github.com>
2024-12-29 16:54:42 +0530

    I've not been missing company, so cleaning it up.

--- core/core-completion.el
+++ core/core-completion.el
@@ -162,97 +162,6 @@
                                            try-complete-lisp-symbol))
   :bind ("M-/" . hippie-expand))
 
-
-(use-package company
-  :disabled t
-  :ensure company-quickhelp
-  :hook ((prog-mode . +enable-company)
-         (text-mode . +enable-company)
-         (conf-mode . +enable-company))
-  :bind ("C-." . company-complete)
-  :init
-  (defmacro company-backend-for-hook(hook backends)
-    `(add-hook ,hook (lambda()
-                       (set (make-local-variable 'company-backends)
-                            ,backends))))
-
-  :config
-  (setq company-idle-delay 0.01
-        company-minimum-prefix-length 2
-        company-require-match 'never
-        company-tooltip-limit 10
-        company-global-modes '(not erc-mode message-mode help-mode gud-mode)
-        ;; company-backends
-        ;; (quote
-        ;;  (company-semantic company-clang company-capf company-files
-        ;;                    (company-dabbrev-code company-keywords)
-        ;;                    company-dabbrev))
-        company-backends '(company-capf company-yasnippet)
-        company-frontends '(company-pseudo-tooltip-frontend)
-        company-auto-commit nil
-        company-auto-commit-chars nil
-        company-dabbrev-ignore-case nil
-        company-dabbrev-downcase nil)
-  ;; (global-company-mode)
-  (defun +enable-company()
-    "Only enable company mode if the buffer is not huge"
-    (company-mode (if (< (buffer-size) 1000000) t -1)))
-  :diminish "Ⓒ")
-
-(use-package company-box
-  :diminish
-  :after company
-  :hook (company-mode . company-box-mode)
-  :config
-  (setq company-box-icons-alist 'company-box-icons-images
-        company-box-show-single-candidate t
-        company-box-backends-colors nil
-        company-box-icons-alist 'company-box-icons-nerd-icons
-        company-box-icons-functions
-        (cons #'+company-box-icons--elisp-fn
-              (delq 'company-box-icons--elisp
-                    company-box-icons-functions))
-        company-box-icons-nerd-icons
-        ;; Courtesy: doom emacs
-        `((Unknown        . ,(nerd-icons-codicon  "nf-cod-code"                :face  'font-lock-warning-face))
-          (Text           . ,(nerd-icons-codicon  "nf-cod-text_size"           :face  'font-lock-doc-face))
-          (Method         . ,(nerd-icons-codicon  "nf-cod-symbol_method"       :face  'font-lock-function-name-face))
-          (Function       . ,(nerd-icons-codicon  "nf-cod-symbol_method"       :face  'font-lock-function-name-face))
-          (Constructor    . ,(nerd-icons-codicon  "nf-cod-triangle_right"      :face  'font-lock-function-name-face))
-          (Field          . ,(nerd-icons-codicon  "nf-cod-symbol_field"        :face  'font-lock-variable-name-face))
-          (Variable       . ,(nerd-icons-codicon  "nf-cod-symbol_variable"     :face  'font-lock-variable-name-face))
-          (Class          . ,(nerd-icons-codicon  "nf-cod-symbol_class"        :face  'font-lock-type-face))
-          (Interface      . ,(nerd-icons-codicon  "nf-cod-symbol_interface"    :face  'font-lock-type-face))
-          (Module         . ,(nerd-icons-codicon  "nf-cod-file_submodule"      :face  'font-lock-preprocessor-face))
-          (Property       . ,(nerd-icons-codicon  "nf-cod-symbol_property"     :face  'font-lock-variable-name-face))
-          (Unit           . ,(nerd-icons-codicon  "nf-cod-symbol_ruler"        :face  'font-lock-constant-face))
-          (Value          . ,(nerd-icons-codicon  "nf-cod-symbol_field"        :face  'font-lock-builtin-face))
-          (Enum           . ,(nerd-icons-codicon  "nf-cod-symbol_enum"         :face  'font-lock-builtin-face))
-          (Keyword        . ,(nerd-icons-codicon  "nf-cod-symbol_keyword"      :face  'font-lock-keyword-face))
-          (Snippet        . ,(nerd-icons-codicon  "nf-cod-symbol_snippet"      :face  'font-lock-string-face))
-          (Color          . ,(nerd-icons-codicon  "nf-cod-symbol_color"        :face  'success))
-          (File           . ,(nerd-icons-codicon  "nf-cod-symbol_file"         :face  'font-lock-string-face))
-          (Reference      . ,(nerd-icons-codicon  "nf-cod-references"          :face  'font-lock-variable-name-face))
-          (Folder         . ,(nerd-icons-codicon  "nf-cod-folder"              :face  'font-lock-variable-name-face))
-          (EnumMember     . ,(nerd-icons-codicon  "nf-cod-symbol_enum_member"  :face  'font-lock-builtin-face))
-          (Constant       . ,(nerd-icons-codicon  "nf-cod-symbol_constant"     :face  'font-lock-constant-face))
-          (Struct         . ,(nerd-icons-codicon  "nf-cod-symbol_structure"    :face  'font-lock-variable-name-face))
-          (Event          . ,(nerd-icons-codicon  "nf-cod-symbol_event"        :face  'font-lock-warning-face))
-          (Operator       . ,(nerd-icons-codicon  "nf-cod-symbol_operator"     :face  'font-lock-comment-delimiter-face))
-          (TypeParameter  . ,(nerd-icons-codicon  "nf-cod-list_unordered"      :face  'font-lock-type-face))
-          (Template       . ,(nerd-icons-codicon  "nf-cod-symbol_snippet"      :face  'font-lock-string-face))
-          (ElispFunction  . ,(nerd-icons-codicon  "nf-cod-symbol_method"       :face  'font-lock-function-name-face))
-          (ElispVariable  . ,(nerd-icons-codicon  "nf-cod-symbol_variable"     :face  'font-lock-variable-name-face))
-          (ElispFeature   . ,(nerd-icons-codicon  "nf-cod-globe"               :face  'font-lock-builtin-face))
-          (ElispFace      . ,(nerd-icons-codicon  "nf-cod-symbol_color"        :face  'success))))
-  (defun +company-box-icons--elisp-fn (candidate)
-    (when (derived-mode-p 'emacs-lisp-mode)
-      (let ((sym (intern candidate)))
-        (cond ((fboundp sym)  'ElispFunction)
-              ((boundp sym)   'ElispVariable)
-              ((featurep sym) 'ElispFeature)
-              ((facep sym)    'ElispFace))))))
-
 (use-package corfu
   :defer 1
   :config
@@ -360,9 +269,6 @@
          ("<tab>" . 'copilot-accept-completion)
          ("M-f" . 'copilot-accept-completion-by-word)
          ("M-<return>" . 'copilot-accept-completion-by-line))
-  :hook ((prog-mode . copilot-mode)
-         (text-mode . copilot-mode)
-         (conf-mode . copilot-mode))
   :config
   ;; Sometimes the copilot agent doesn't start. Restarting fixes the issue.
   (copilot-diagnose)
