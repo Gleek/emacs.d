d049f0e090d204e2ec7308059a279fc67359bcad
Umar Ahmad <Gleek@users.noreply.github.com>
2025-06-29 17:10:27 +0530

    Some tramp optimisations
    Based on https://coredumped.dev/2025/06/18/making-tramp-go-brrrr./

--- core/core-tramp.el
+++ core/core-tramp.el
@@ -7,17 +7,23 @@
   ;; (setq tramp-shell-prompt-pattern "^[^$>\n]*[#$%>] *\\(\[[0-9;]*[a-zA-Z] *\\)*")
   :config
   (setq remote-file-name-inhibit-cache 60
+        remote-file-name-inhibit-locks t
+        remote-file-name-inhibit-auto-save-visited t
+        tramp-copy-size-limit (* 1024 1024) ;; 1MB
         tramp-verbose 1
         vc-ignore-dir-regexp (format "%s\\|%s\\|%s"
                                      vc-ignore-dir-regexp
                                      tramp-file-name-regexp
                                      "[/\\\\]node_modules"))
-  (add-to-list 'tramp-default-proxies-alist
-               '("jobs" nil "/ssh:entry:"))
-  (add-to-list 'tramp-default-proxies-alist
-               '("scripts" nil "/ssh:entry:"))
-  (add-to-list 'tramp-default-proxies-alist
-               '("aa102" nil "/ssh:entry:")))
+  (connection-local-set-profile-variables
+   'remote-direct-async-process
+   '((tramp-direct-async-process . t)))
+
+  (connection-local-set-profiles
+   '(:application tramp :protocol "scp")
+   'remote-direct-async-process)
+
+  (setq magit-tramp-pipe-stty-settings 'pty))
 
 ;; (use-package tramp-container)
 (provide 'core-tramp)
